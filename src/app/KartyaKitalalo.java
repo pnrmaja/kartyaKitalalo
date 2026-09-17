e80f646cf9920c875202c6d4e71e34ad6fa92867
pnrmaja <ponauerszilvi1@gmail.com>
2026-09-17 15:10:09 +0200

    eretkul adas

--- src/app/KartyaKitalalo.java
+++ src/app/KartyaKitalalo.java
@@ -135,6 +135,8 @@ public class KartyaKitalalo {
             default:
                 throw new AssertionError();
         }
+        pakli = uj;
+        
     }
  
     private static void EzVolt() {
